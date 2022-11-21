cfc372fff8c66ce71bff2aab8ff70b39f1856db0
amrut <amrut@AmruthaPC>
2022-11-21 13:03:52 +0530

    framework

---
+++ src/test/java/script/Test1.java
@@ -0,0 +1,30 @@
+package script;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+@Test(groups = "smoke")
+	
+	public void script1()
+	{
+		System.out.println("........script 1............");
+	}
+	
+	@Test()
+	public void script2()
+	{
+		System.out.println("........script 2............");
+	}
+	
+	@Test()
+	public void script11()
+	{
+		System.out.println("........script 11............");
+	}
+	@Test()
+	public void script22()
+	{
+		System.out.println("........script 22............");
+	}
+}
