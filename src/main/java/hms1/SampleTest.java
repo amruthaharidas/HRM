e3a890fbd0a77e83298689273b197172c3054d9b
dell <dell@mypc>
2022-11-22 12:44:00 -0500

    commite1

---
+++ src/main/java/hms1/SampleTest.java
@@ -0,0 +1,16 @@
+package hms1;
+public class SampleTest 
+{
+
+
+
+	   public void script1()
+	   {
+		   System.out.println("script1");  
+	   }
+	
+	   public void script2()
+	   {
+		   System.out.println("script2");  
+	   }
+}
