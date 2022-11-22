3b59f9681416a90b83df2d4b72b3f528947acbc8
dell <dell@mypc>
2022-11-22 12:53:35 -0500

    com

---
+++ SampleTest.java
@@ -0,0 +1,19 @@
+package PRACTICE2;
+
+import org.testng.annotations.Test;
+
+public class SampleTest 
+{
+
+
+	  @Test()
+	   public void script1()
+	   {
+		   System.out.println("script1");  
+	   }
+	  @Test()
+	   public void script2()
+	   {
+		   System.out.println("script2");  
+	   }
+}
